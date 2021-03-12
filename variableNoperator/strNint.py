8e9ce8e8ca84947e7e3db7b2d5fb6df7f1af75a7
fbgkdn2484 <80086225+fbgkdn2484@users.noreply.github.com>
2021-03-12 16:06:31 +0900

    Add files via upload

---
+++ variableNoperator/strNint.py
@@ -0,0 +1,10 @@
+# 문자열 입력
+a = input("첫번 째 숫자 : ")
+b = input("두번 째 숫자 : ")
+print(a+b)
+print("Hello" + "Python")
+
+#문자열을 숫자로 변환시키는 한수 int
+a = int(input("첫번 째 숫자 : "))
+b = int(input("두번 째 숫자 : "))
+print(a+b)
