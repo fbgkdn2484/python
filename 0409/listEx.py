30a6375eb49218d0f7784ceacdc9274d518cdfad
fbgkdn2484 <80086225+fbgkdn2484@users.noreply.github.com>
2021-04-09 15:48:48 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,14 @@
+fstName = ['R', 'y', 'u']
+sndName = ["Ha", "woo"]
+stdName = [2, 0, 2, 1, 0, 6, 0, 4, 2]
+
+print(fstName)
+print(sndName)
+print(stdName)
+
+# 화면에 H, G, D를 출력하는 프로그램을 작성하시오.
+print(fstName[0])   # H
+                    # G
+                    # D
+
+
